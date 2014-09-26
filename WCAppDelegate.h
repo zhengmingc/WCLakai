7f09532f38e902f4b020376cedb9522868479201
Zhengming Chu <chuzhengming@gmail.com>
2014-09-26 16:16:59 -0700

    Initial commit

---
+++ WCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WCAppDelegate.h
+//  WCLakai
+//
+//  Created by Wenwen on 2014-09-26.
+//  Copyright (c) 2014 com.wenwenchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WCLakai
+//
+//  Created by Wenwen on 2014-09-26.
+//  Copyright (c) 2014 com.wenwenchu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WCAppDelegate class]));
+    }
+}
